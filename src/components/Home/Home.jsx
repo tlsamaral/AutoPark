f1c02bf0552af505fe4d9f663f896e143941e7bd
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-04-24 00:33:23 -0300

    Inicio da conexao socket-client

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
+import io from 'socket.io-client';
 import ListCardVanancy from '../ListCardVanacy/ListCardVanancy';
 
 function Home() {
+  useEffect(() => {
+    const socket = io('localhost:3001');
+    console.log(socket);
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+  }, []);
   return (
     <div className="w-full h-full">
       <ListCardVanancy />
