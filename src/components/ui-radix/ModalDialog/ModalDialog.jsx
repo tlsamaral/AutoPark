ea11fc2fc48b5fd7f1565bf8bc8c697874366f30
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-05-05 13:58:45 -0300

    Corrigindo button add new Vacancies

--- src/components/ui-radix/ModalDialog/ModalDialog.jsx
+++ src/components/ui-radix/ModalDialog/ModalDialog.jsx
@@ -4,11 +4,27 @@ import PropTypes from 'prop-types';
 import AppContext from '../../../context/AppContext';
 
 function ModalDialog({ buttonText, description, children }) {
-  const { dialogOpen, setDialogOpen } = useContext(AppContext);
+  const { dialogOpen, setDialogOpen, setSelectedVacancy } =
+    useContext(AppContext);
+
+  const handleClick = () => {
+    setSelectedVacancy({
+      id: 0,
+      description: '',
+      sensor_id: '',
+      port_r: '',
+      port_g: '',
+      port_b: '',
+      micro_id: '',
+      id_occupied: false,
+    });
+  };
   return (
     <Dialog.Root open={dialogOpen} onOpenChange={setDialogOpen}>
       <Dialog.Trigger>
-        <Button color="gray">{buttonText}</Button>
+        <Button color="gray" onClick={handleClick}>
+          {buttonText}
+        </Button>
       </Dialog.Trigger>
 
       <Dialog.Content maxWidth="450px">
