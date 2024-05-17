da597df4e271bac9ce95310614d85c18ebeae803
talles.amaral <worktalles@hotmail.com>
2024-05-17 08:33:57 -0300

    Enviando atualizações da page 404

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -1,7 +1,13 @@
+import { HiLocationMarker } from 'react-icons/hi';
+
 function Page404() {
   return (
-    <div className="w-full h-full">
-      <h1>Página não encontrada</h1>
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="flex items-center">
+        <HiLocationMarker size={68} className="text-red-600" />
+        <h1 className="text-6xl">404</h1>
+      </div>
+      <h1 className="text-3xl">Página não encontrada</h1>
     </div>
   );
 }
