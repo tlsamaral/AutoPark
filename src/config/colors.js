bfdf97e5cd63a351cc0bdce7efc1e950c20cc566
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-05-28 18:55:44 -0300

    Alterando arquivos colors

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,6 +1,7 @@
 export const primaryColor = '#C3073F';
 export const primaryDarkColor = '#1A1A1D';
 
+// Variant colors
 export const successColor = '#0197F6';
 export const infoColor = '#01F97F6';
 export const errorColor = '#F2AF29';
